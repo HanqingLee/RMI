3dee5a154dc42a50bbd64a5100ce1c28f3d03e22
Hanson Lee <hanqingdyx@gmail.com>
2017-04-28 17:26:07 +0800

    Add files via upload
    auto-create sql table

--- src/test/RMIPracticeImpl.java
+++ src/test/RMIPracticeImpl.java
@@ -28,7 +28,11 @@ public class RMIPracticeImpl implements RMIPractice {
 			//initialize table
 			sql = "create table USER_INFO( id INT not null AUTO_INCREMENT,name VARCHAR(50) not null, password VARCHAR(50) not null,primary key(ID));";
 			statement.execute(sql);
-
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
 			// check if there has existed the same user name
 			ResultSet rs = null;
 			sql = "select * from USER_INFO where name='" + username + "'";
@@ -67,7 +71,11 @@ public class RMIPracticeImpl implements RMIPractice {
 			//initialize table
 			sql = "create table USER_INFO( id INT not null AUTO_INCREMENT,name VARCHAR(50) not null, password VARCHAR(50) not null,primary key(ID));";
 			statement.execute(sql);
-
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
 			// check if there is the user name required
 			ResultSet rs = null;
 			sql = "select * from USER_INFO where name='" + username + "'";
